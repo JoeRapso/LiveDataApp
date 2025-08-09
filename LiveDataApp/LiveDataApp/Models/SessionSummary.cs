08646707c615b8008b53d22f22e91f1f70e816d5
joerapso <joe.rapso@gmail.com>
2025-08-09 13:47:43 +0100

    Initial setup, created SessionSummary model

---
+++ LiveDataApp/LiveDataApp/Models/SessionSummary.cs
@@ -0,0 +1,9 @@
+﻿namespace LiveDataApp.Models;
+
+public record SessionSummary(Guid SessionId, 
+    string Series, 
+    string Name, 
+    string Track, 
+    string State, 
+    DateTime StartTime, 
+    TimeSpan Duration);
